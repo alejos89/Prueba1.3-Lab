c9eaff50002cf083ffbacf8e766af51be21d1045
alejoss <aggomez@scl.edu.gt>
2025-08-12 18:18:34 -0600

    Login Register

--- componentes/banner2/banner2Component.js
+++ componentes/banner2/banner2Component.js
@@ -1,43 +1,48 @@
-export function banner2(){
+import { section2 } from "../logInNo2/logIn.js";
+
+export function banner2() {
     let div = document.createElement('div');
     div.className = "div-banner2";
 
     let h2 = document.createElement("h2");
     h2.textContent = "Geeta.";
     h2.className = "h2";
-    h2.style.fontWeight = "bold"; // Negrita
+    h2.style.fontWeight = "bold";
     div.appendChild(h2);
 
     let p1 = document.createElement("p");
     p1.textContent = "Create your fashion in your own way";
     p1.className = "p1";
-    p1.style.fontWeight = "bold"; // Negrita
+    p1.style.fontWeight = "bold";
     div.appendChild(p1);
 
     let p2 = document.createElement("p");
-    p2.textContent = "Each men and women has their own style, Geeta help you to create your unique style. ";
+    p2.textContent = "Each men and women has their own style, Geeta help you to create your unique style.";
     p2.className = "p2";
     div.appendChild(p2);
 
     let logIn = document.createElement("a");
     logIn.textContent = "Log In";
     logIn.className = "log-in";
-    logIn.style.fontWeight = "bold"; // Negrita
-    logIn.addEventListener("click", () => { alert("Hola mundo")
-      div.classList.add("ocultar");
-     });
+    logIn.style.fontWeight = "bold";
+    logIn.href = "#";
+    logIn.addEventListener("click", (event) => {
+        event.preventDefault(); // Evita recarga
+        div.classList.add("ocultar"); 
+        document.body.appendChild(section2()); 
+    });
     div.appendChild(logIn);
 
     let orSpan = document.createElement("span");
     orSpan.textContent = "--Or-- ";
     orSpan.className = "or-Span";
-    orSpan.style.fontWeight = "bold"; // Negrita
+    orSpan.style.fontWeight = "bold";
     div.appendChild(orSpan);
 
     let registerBtn = document.createElement("a");
     registerBtn.textContent = "Register";
     registerBtn.className = "register-Btn";
-    registerBtn.style.fontWeight = "bold"; // Negrita
+    registerBtn.style.fontWeight = "bold";
     div.appendChild(registerBtn);
 
     return div;
